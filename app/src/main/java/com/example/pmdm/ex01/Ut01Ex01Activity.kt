87113bb9934b932bd8b76dc446deee50f7364fd8
esteban gamboa <estebitangb12@gmail.com>
2022-10-03 10:16:08 +0200

    Second exercise.

---
+++ app/src/main/java/com/example/pmdm/ex01/Ut01Ex01Activity.kt
@@ -0,0 +1,11 @@
+package com.example.pmdm.ex01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex01)
+    }
+}
---
+++ app/src/main/java/com/example/pmdm/ex02/presentation/Ut01Ex02bActiviry.kt
@@ -0,0 +1,12 @@
+package com.example.pmdm.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pmdm.R
+
+class Ut01Ex02bActiviry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex02b_activiry)
+    }
+}
